16bba21830bf4df39b8ff2ef56bccf2341b12772
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2025-02-25 03:42:24 +0700

    install script fixes, update packages

--- packages/iot4b_install.sh
+++ packages/iot4b_install.sh
@@ -9,7 +9,7 @@ NDMQ_PKG="ndmq_1.0.2-7_mipsel-3.4_kn.ipk"
 if [ -f /etc/openwrt_release ]; then
     echo "Detected OpenWRT."
     echo "Downloading package..."
-    wget -O /tmp/$OPENWRT_PKG "$PKG_URL/$OPENWRT_PKG" || { echo "Failed to download iot4b_openwrt"; exit 1; }
+    curl -o /tmp/$OPENWRT_PKG "$PKG_URL/$OPENWRT_PKG" || { echo "Failed to download $OPENWRT_PKG"; exit 1; }
     echo "Installing package..."
     opkg install /tmp/$OPENWRT_PKG
     echo "Cleaning up..."
@@ -18,9 +18,9 @@ if [ -f /etc/openwrt_release ]; then
 elif grep -qi "NDMS" /proc/version; then
     echo "Detected Keenetic."
     echo "Downloading packages..."
-    curl -o /tmp/$LIBNDM_PKG $PKG_URL/$LIBNDM_PKG || { echo "Failed to download libndm"; exit 1; }
-    curl -o /tmp/$NDMQ_PKG $PKG_URL/$NDMQ_PKG || { echo "Failed to download ndmq"; exit 1; }
-    curl -o /tmp/$KEENETIC_PKG $PKG_URL/$KEENETIC_PKG || { echo "Failed to download iot4b_keenetic"; exit 1; }
+    curl -o /tmp/$LIBNDM_PKG "$PKG_URL/$LIBNDM_PKG" || { echo "Failed to download $LIBNDM_PKG"; exit 1; }
+    curl -o /tmp/$NDMQ_PKG "$PKG_URL/$NDMQ_PKG" || { echo "Failed to download $NDMQ_PKG"; exit 1; }
+    curl -o /tmp/$KEENETIC_PKG "$PKG_URL/$KEENETIC_PKG" || { echo "Failed to download $KEENETIC_PKG"; exit 1; }
     echo "Installing packages..."
     opkg install /tmp/$LIBNDM_PKG
     opkg install /tmp/$NDMQ_PKG
